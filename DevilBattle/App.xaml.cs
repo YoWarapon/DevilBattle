172ab7c6ff75b5b626cbd61b33f4926ce9265ae5
yoWarapon <warapon.work@gmail.com>
2025-04-06 19:37:52 +0700

    Add project files.

---
+++ DevilBattle/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DevilBattle
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
