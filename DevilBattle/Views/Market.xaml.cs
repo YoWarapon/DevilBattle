e0fdae96ef00e8aca5d70c5ed8ea19091aef1e75
Mingchai007 <007gwz2546@gmail.com>
2025-04-06 23:26:12 +0700

    Market

--- DevilBattle/Views/Market.xaml.cs
+++ DevilBattle/Views/Market.xaml.cs
@@ -6,4 +6,33 @@ public partial class Market : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnMinClicked(object sender, EventArgs e)
+    {
+        AmountEntry.Text = "1";
+    }
+
+    private void OnMaxClicked(object sender, EventArgs e)
+    {
+        AmountEntry.Text = "9999"; 
+    }
+
+    private void OnCancelAmount(object sender, EventArgs e)
+    {
+        AmountPopup.IsVisible = false;
+    }
+
+    private void OnConfirmAmount(object sender, EventArgs e)
+    {
+        int amount = int.Parse(AmountEntry.Text);
+        AmountPopup.IsVisible = false;
+    }
+
+    private void OnItemTapped(object sender, EventArgs e)
+    {
+        AmountPopup.IsVisible = true;
+        AmountEntry.Text = "1"; 
+    }
+
+
 }
